a42ffa93dc8413bfb135ef6f6d19a3f844e5be44
PehrGit <trickshotpeter@gmail.com>
2013-11-08 15:29:27 +0100

    You ask no money, you are old man

---
+++ OldManOPA/src/oldmanopa/OldManOPA.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oldmanopa;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author Pehr
+ */
+public class OldManOPA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame = new JFrame("How, to pay here for?");
+        frame.setBounds(800, 100, 500, 100);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        
+        System.out.println("Yes yes yes girl");
+        JOptionPane.showMessageDialog(frame, "Yes yes yes girl");
+    }
+    
+}
