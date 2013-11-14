862e64a6ec81c55a0dc5d07a503c955b77feeead
PehrGit <trickshotpeter@gmail.com>
2013-11-14 12:51:04 +0100

    Beginnetje main

--- OldManOPA/src/nl/hmh/OldManOPA/Main.java
+++ OldManOPA/src/nl/hmh/OldManOPA/Main.java
@@ -6,6 +6,12 @@
 
 package nl.hmh.OldManOPA;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Pehr
@@ -15,6 +21,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        while(true){            
+            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                String s = bufferRead.readLine();
+                System.out.println("you typed: " + s);
+                
+                if(s.equals("exit")){
+                    System.out.println("Goodbye");
+                    System.exit(0);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                System.exit(1);
+            }
+        }
     }
 }
